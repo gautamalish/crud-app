6aad01660a293942aa57105422f845bbf46ecfba
zyzzchad <119152524+gautamalish@users.noreply.github.com>
2024-05-12 06:28:15 +0545

    Firstcommit

---
+++ src/config/firestore.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAqc0AGOOObUDKEg6a3_KSODfpX8KZU1eg",
+  authDomain: "movie-review-5ef60.firebaseapp.com",
+  projectId: "movie-review-5ef60",
+  storageBucket: "movie-review-5ef60.appspot.com",
+  messagingSenderId: "590520819695",
+  appId: "1:590520819695:web:25d5661eb0127fa385201a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
